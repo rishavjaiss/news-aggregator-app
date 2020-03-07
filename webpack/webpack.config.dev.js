d697cac5b0a597f24d2ce6ecc059fb404d16653b
Rishav Jaiswal <rishavjaiss14@gmail.com>
2020-03-07 17:25:23 +0530

    Completed

--- webpack/webpack.config.dev.js
+++ webpack/webpack.config.dev.js
@@ -1,42 +1,46 @@
-const Path = require('path');
-const Webpack = require('webpack');
-const merge = require('webpack-merge');
-const common = require('./webpack.common.js');
+const Path = require("path");
+const Webpack = require("webpack");
+const merge = require("webpack-merge");
+const common = require("./webpack.common.js");
 
 module.exports = merge(common, {
-  mode: 'development',
-  devtool: 'cheap-eval-source-map',
+  mode: "development",
+  devtool: "cheap-eval-source-map",
   output: {
-    chunkFilename: 'js/[name].chunk.js'
+    chunkFilename: "js/[name].chunk.js"
   },
   devServer: {
     inline: true,
-    port:3000
+    port: 3000
   },
   plugins: [
     new Webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify('development')
+      "process.env.NODE_ENV": JSON.stringify("development")
+    }),
+    new webpack.ProvidePlugin({
+      $: "jquery",
+      jQuery: "jquery"
     })
   ],
   module: {
     rules: [
       {
         test: /\.(js)$/,
-        include: Path.resolve(__dirname, '../src'),
-        enforce: 'pre',
-        loader: 'eslint-loader',
+        include: Path.resolve(__dirname, "../src"),
+        enforce: "pre",
+        loader: "eslint-loader",
         options: {
-          emitWarning: true,
+          emitWarning: true
         }
       },
       {
         test: /\.(js)$/,
-        include: Path.resolve(__dirname, '../src'),
-        loader: 'babel-loader'
+        include: Path.resolve(__dirname, "../src"),
+        loader: "babel-loader"
       },
       {
         test: /\.s?css$/i,
-        use: ['style-loader', 'css-loader?sourceMap=true', 'sass-loader']
+        use: ["style-loader", "css-loader?sourceMap=true", "sass-loader"]
       }
     ]
   }
